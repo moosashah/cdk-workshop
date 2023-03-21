5e4a9f38716b829572d04655c311ad0e02128954
Moosa Shah <hello.moosashah@gmail.com>
2023-03-21 17:29:03 +0000

    commit some js files

---
+++ lamda/hitcounter.js
@@ -0,0 +1,27 @@
+const { DynamoDB, Lambda } = require('aws-sdk')
+
+exports.handler = async function (event) {
+  console.log('Request:', JSON.stringify(event, undefined, 2))
+
+  const dynamo = new DynamoDB()
+  const lamda = new Lambda()
+
+  await dynamo
+    .updateItem({
+      TableName: process.env.HITS_TABLE_NAME,
+      Key: { path: { S: event.path } },
+      UpdateExpression: 'ADD hits :incr',
+      ExpressionAttributeValues: { ':incr': { N: '1' } },
+    })
+    .promise()
+
+  const resp = await lamda
+    .invoke({
+      FunctionName: process.env.DOWNSTREAM_FUNCTION_NAME,
+      Payload: JSON.stringify(event),
+    })
+    .promise()
+  console.log('downstream response:', JSON.stringify(resp, undefined, 2))
+
+  return JSON.parse(resp.Payload)
+}
